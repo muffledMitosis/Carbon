5699eec003adef3307d86a3c10d3b9ae073da448
Meth Munindradasa <methsndr@gmail.com>
2022-10-12 12:04:27 +0530

    Add file utilities

---
+++ src/Engine/Utils/FileUtils.cpp
@@ -0,0 +1,38 @@
+#include "FileUtils.hpp"
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+namespace Utils
+{
+// TODO: Implement file accessibity check
+bool isAccessible(fs::path path) { return false; }
+
+std::string readFile(fs::path path)
+{
+	std::ifstream ifs;
+	std::string fileContents = "";
+	std::string temp = "";
+
+	if(fs::exists(path) && fs::is_regular_file(path))
+	{
+		ifs.open(path, std::ios::in);
+		if(ifs)
+		{
+			while(!ifs.eof())
+			{
+				std::getline(ifs, temp);
+				fileContents += temp + "\n";
+			}
+		}
+		ifs.close();
+		return fileContents;
+	} else
+	{
+		return "not a regular file";
+	}
+}
+}		// namespace Utils
---
+++ src/Engine/Utils/FileUtils.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "../Core.h"
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+namespace Utils
+{
+std::string C_API readFile(fs::path path);
+bool C_API isAccessible(fs::path path);
+}		// namespace Utils
